4ac1363f5faaebc59fa1fbd10836c75477b94dd7
madhavsharma300905 <madhav300905@gmail.com>
2025-08-03 18:45:41 +0530

    Datatypes-Interview-Questions

---
+++ datatypes-summary.js
@@ -0,0 +1,30 @@
+// The datatypes are basically divided into two types on the basis of their value is stored in the memory and accessed from the memory.
+
+// Interview related questions
+// - primitive and Nonprimitive (call by value, call by reference)
+// - primitive:7 (call by value)
+// - string, number, boolean, null(Empty), undefined, symbol , BigInt
+// - Nonprimitive:3 (call by reference)
+// - arrays, object, function
+
+// - Dynamically type vs statically type - js is dynamically typed
+
+//In Primitive datatype they are called by value datatypes that means you can perform changes on the copied value original value remain unchanged
+//In NonPrimitive datatype they  are called by reference datatypes that means you perform changes on the original value and the original value get changed.
+
+// Primitive data type goes to Stack we get a copy of that value.
+// Non-Primitive data type goes to Heap we get reference of that value.
+
+/*1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object*/
